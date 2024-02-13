91f99bf679ac99d04d987d2d54f8453eb8e3815a
Eren Tın <erentin@outlook.com>
2024-02-13 12:18:18 +0300

    Length > 2 condition added to searchValue.

--- app/(authenticated)/movies/page.js
+++ app/(authenticated)/movies/page.js
@@ -9,12 +9,11 @@ async function Movies({searchParams}) {
 
   const searchValue = searchParams.search;
   const searchedMovies = await fetchMovieSearch(searchValue);
-  console.log(searchedMovies)
   
   return (
     <>
     {
-      searchValue && 
+      searchValue &&  searchValue.length> 2 && 
       <div className='absolute bg-custom-gray h-[100%] w-full z-[1000] py-[15%] lg:py-[5%]'>
             <div className='flex flex-col gap-10 px-[15%] sm:px-[5%] lg:pb-20'>
 
