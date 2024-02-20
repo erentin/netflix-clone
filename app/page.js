e6db67ace3f276831692b277bcaaa8d2dc359f3b
Eren Tın <erentin@outlook.com>
2024-02-20 10:24:11 +0300

    Middleware updated.

--- app/page.js
+++ app/page.js
@@ -4,7 +4,17 @@ import Header from "@/components/Global/Header/Header";
 import Hero from "@/components/HomeGuest/Hero/Hero";
 import HomeGuest from "@/containers/HomeGuest/HomeGuest";
 
+import { redirect } from 'next/navigation'; 
+import { auth } from '@clerk/nextjs';
+
 export default function Home() {
+  const {userId} = auth();
+
+  if (userId) {
+    redirect('/movies')
+  }
+
+
   return (
     <main>
       <Header />
--- middleware.js
+++ middleware.js
@@ -1,24 +1,7 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-import { NextResponse } from "next/server"; 
 
 export default authMiddleware({
-
- 
   publicRoutes: ["/"],
-
-  afterAuth(auth, req, evt) {
-
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-
-    if (auth.userId && auth.isPublicRoute) {
-      const orgSelection = new URL("/movies", req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-
-  },
-  
 });
  
 export const config = {
